e081ec12b6c4194218687389ab0b673439bbbb4c
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2026-09-15 06:55:41 +0530

    initial commit on loki setup

---
+++ grafana-loki/variables.tf
@@ -0,0 +1,8 @@
+variable "ami_id" {}
+variable "instance_spec" {}
+variable "sshkey" {}
+variable "virtualfirewall" {}
+variable "project_name" {}
+variable "aws_region" {}
+variable "environment" {}
+variable "owner" {}
